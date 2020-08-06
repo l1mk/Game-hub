75bbe3fca0198e6ab69ea4a6dde7ca39d87bb80d
github username <github email address>
2020-08-06 18:35:08 -0500

    added score in the html and both fetch and rendering score function on my js

--- pkm-frontend/src/index.js
+++ pkm-frontend/src/index.js
@@ -1,12 +1,14 @@
 console.log("testing...")
+//variable declaration
 const player_form = document.getElementById("create-player");
 const login_info = document.getElementById("login-info");
 const player_name = document.getElementById("player-name");
-
+const score_div = document.getElementById("score-div")
+const scores_table = document.getElementsByClassName("score-table")[0]
+let scores = [];
+//After dom load initial actions
 document.addEventListener("DOMContentLoaded", () => {
-
-
-
+    //submit form action
     player_form.addEventListener("submit", function(a){
         a.preventDefault();
         console.log('submit pressed')
@@ -14,12 +16,20 @@ document.addEventListener("DOMContentLoaded", () => {
       });
 })
 
+//function declartion: rendering of player
 function renderPlayer(name){
     console.log('rendering', name)
     player_name.innerHTML = name
     login_info.classList.remove('hidden')
+    player_form.classList.add('hidden')
+    renderScore()
 }
-
+//function declartion: rendering of scores
+function renderScore(){
+score_div.classList.remove('hidden')
+fetchScore()
+}
+//function declaration: new player creation
 function fetchNewPlayer(name){
     let configObj = {
       method: "POST",
